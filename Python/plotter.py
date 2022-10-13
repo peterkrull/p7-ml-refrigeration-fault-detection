50064e56bacd846eafc6aaef341cc929dbbba3b1
LauLauridsen <lau.lauridsen@hotmail.com>
2022-10-13 08:46:37 +0200

    New dataset

--- Python/plotter.py
+++ Python/plotter.py
@@ -3,11 +3,16 @@ import numpy as np
 from pandas import read_csv
 import pandas as pd
 
-data = pd.DataFrame(read_csv("TrainingData/Ftrain19.csv")[0:6000],dtype=float)
+data = pd.DataFrame(read_csv('Ftrain4.csv')[0:6000],dtype=float)
 label = pd.DataFrame({'label':[0 for _ in range(len(data))]})
 index = pd.DataFrame({'index':[x for x in range(len(data))]})
 
+
+data['CprPower'] = data['CprPower']/1000
+
+
 xdata = index.join(data).join(label)
+
 
 ran = [0,6000]
 
