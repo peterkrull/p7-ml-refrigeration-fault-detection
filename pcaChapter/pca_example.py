d2af44c94d9d043bb31a5f4fdc18d2ffc8bdddf1
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-10-19 13:21:58 +0200

    Updated PCA chapter figures

--- pcaChapter/pca_example.py
+++ pcaChapter/pca_example.py
@@ -10,26 +10,37 @@ plt.scatter(data[0,:].tolist(), data[1,:].tolist())
 
 sample_mean = data.mean(1)
 sample_cov = np.cov(data)
+print("sample_mean: \n" + str(sample_mean))
 
 eig_val, eig_vec = np.linalg.eig(sample_cov)
-print(eig_vec)
+print("\neig vectors: \n" + str(eig_vec))
+print("\neig_val:\n" + str(eig_val))
 w = eig_vec[:,1]
-print(w)
+print("\nw: \n" + str(w))
 
+print("\ndata: \n" + str(data))
+print("\ndata-sample_mean: \n" + str(data-sample_mean))
 transformed_data = np.matmul(np.matrix(w),data-sample_mean)
-print(transformed_data)
+print("\ntransformed_data: \n" + str(transformed_data))
 
-transformed_coordinate = np.matmul(np.matrix(w).T, np.matrix(transformed_data))
-print(transformed_coordinate)
+print("\n Size data: \n", data.shape)
+print("\n Size w: \n", np.matrix(w).T.shape)
+transformed_coordinate = np.matmul(np.matrix(w), np.matrix(data))
+print("\ntransformed_coordinate: \n" + str(transformed_coordinate))
+print("\n Size transformed_coordinate: \n", np.matrix(transformed_coordinate).shape)
+coordinate_along_w = np.matmul(np.matrix(w).T, np.matrix(transformed_coordinate))
+print("\n coordinate along w: \n", str(coordinate_along_w))
+print("\n Size coordinate_along_w: \n", coordinate_along_w.shape)
 
-
-plt.scatter(transformed_coordinate[0,:].tolist(), transformed_coordinate[1,:].tolist(), color = "orange")
-plt.plot(transformed_coordinate[0,:].T, transformed_coordinate[1,:].T, color = "orange")
+plt.scatter(coordinate_along_w[0,:].tolist(), coordinate_along_w[1,:].tolist(), color = "orange")
+plt.plot(coordinate_along_w[0,:].T, coordinate_along_w[1,:].T, color = "orange")
 
 len_trans = max(transformed_coordinate.shape)
 for i in range(0, len_trans):
-    plt.plot([transformed_coordinate[0,i], data[0,i]], [transformed_coordinate[1,i], data[1,i]], "--", color = "black")
-    print(np.dot([transformed_coordinate[0,i]-data[0,i] ,transformed_coordinate[1,i]-data[1,i]], w))
+    plt.plot([coordinate_along_w[0,i], data[0,i]], [coordinate_along_w[1,i], data[1,i]], "--", color = "black")
+    print(np.dot([coordinate_along_w[0,i]-data[0,i] ,coordinate_along_w[1,i]-data[1,i]], w))
+plt.axis('equal')
 
-tpl.save("pcaChapter/PCA_example_py.tex")
+#plt.show()
+tpl.save("PCA_example_py.tex")
 
