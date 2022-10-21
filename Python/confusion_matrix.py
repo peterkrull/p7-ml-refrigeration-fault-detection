89a0690fe24a9b955ccd69b77b2665118c8f7f28
Peter Krull <peterkrullpeter@gmail.com>
2022-10-21 13:18:30 +0200

    Added accuracy to confusion matrix title

--- Python/confusion_matrix.py
+++ Python/confusion_matrix.py
@@ -2,14 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def confusion_matrix(conf_matrix : np.matrix, axis_ticks : tuple = None, title : str = "Confusion matrix", normalize = True,save_fig_name : str = None, figsize : tuple = (10,10)):
+    
+    # Calculate accuracy of confusion matrix
+    accuracy = np.sum(np.diag(conf_matrix))/np.sum(conf_matrix)
+    
+    # Setup figure
     fig , axs = plt.subplots(figsize=figsize)
     axs.imshow(np.sqrt(conf_matrix),cmap="Greens")
     axs.set_xlabel("Predicted class")
     axs.set_ylabel("True class")
-    axs.set_title(title)
+    axs.set_title(f"{title} : Accuracy {round(accuracy*100,2)}%")
 
     width, height = conf_matrix.shape
 
+    # Apply axis ticks
     if type(axis_ticks) != type(None):
         if len(axis_ticks) == 2:
             axs.set_xticks(axis_ticks[0],axis_ticks[1])
@@ -21,6 +27,7 @@ def confusion_matrix(conf_matrix : np.matrix, axis_ticks : tuple = None, title :
         axs.set_xticks([x for x in range(width)],[x for x in range(width)])
         axs.set_yticks([x for x in range(height)],[x for x in range(height)])
 
+    # Apply numbers to matrix fields
     for x in range(width):
         for y in range(height):
             if normalize:
@@ -32,6 +39,7 @@ def confusion_matrix(conf_matrix : np.matrix, axis_ticks : tuple = None, title :
                     horizontalalignment='center',
                     verticalalignment='center')
                 
+    # Optional, export figure
     if save_fig_name:
         axs.get_figure().savefig(save_fig_name,format='pdf')
         
