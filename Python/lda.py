b4a07f1a90ad4d07f8ac29a7d95bace089712609
Peter Krull <peterkrullpeter@gmail.com>
2022-10-27 21:38:59 +0200

    Added LDA classifier

--- Python/lda.py
+++ Python/lda.py
@@ -57,7 +57,7 @@ class LDA_reducer:
 
         s = sum(np.real(self.eig_val))
         print(f"Preserving {round(sum(np.real(self.eig_val[0:dims]))/s*100,2)}% of variance",)
-    
+
     # Redefine the number of dimensions
     def set_dims(self,dims:int):
         self.W = self.eig_vec[:,0:dims].astype('float')
@@ -76,3 +76,49 @@ class LDA_reducer:
         
         # Return data
         return Z.astype('float')
+
+class LDA_classifier:
+
+    # Initialize the LDA by using a dataset
+    def __init__(self, X : 'pd.ndarray', y : 'pd.ndarray', p: 'pd.ndarray' = None) -> None:
+
+        """Linear Discriminant Classification constructor
+
+        Based on pdf-page 128 from :
+        https://hastie.su.domains/ElemStatLearn/printings/ESLII_print12_toc.pdf
+
+        """
+
+        # Enforce data types
+        X = np.array(X)
+        y = np.squeeze(y)
+        p = np.squeeze(p)
+
+        self.classes = np.unique(y)
+
+        # Extract mean, covariance and priors from data
+        self.M = np.ascontiguousarray( [np.mean(X[y == c],axis=0) for c in self.classes], dtype = float )
+        self.S = np.ascontiguousarray( [np.cov(X[y == c],rowvar=False) for c in self.classes], dtype = float )
+        self.P = np.ascontiguousarray( [len(X[y == c]) for c in self.classes], dtype = float ) if p == None else p 
+
+        # Calculate within-class covariance of dimensionality reduced data
+        self.Sw = np.zeros((X.shape[1],X.shape[1]))
+        for c in np.unique(y):
+            data_k = X[y == c]
+            muk = data_k.mean()  
+            inner = data_k - muk      
+            self.Sw += np.dot(inner.T,inner)
+
+    def lda_classifier(self,X):
+
+            # Get 'average' covariance
+            S = sum(self.S)
+
+            X = np.array(X)
+            return np.argmax([ discriminant_function(X,S,m,p) for m,p in zip(self.M,self.P) ], axis=0)
+
+
+def discriminant_function(X,s,m,p):
+    part1 = np.dot(X,np.dot(np.linalg.inv(s),m ))
+    part2 = 0.5*np.dot(m,np.dot(np.linalg.inv(s),m ))
+    return part1 - part2 + np.log(p)
