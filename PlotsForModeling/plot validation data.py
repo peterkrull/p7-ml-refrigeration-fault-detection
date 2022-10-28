01d286bff449d6b3b7ea4e066e39621875a89618
Mark <klepacsmar@gmail.com>
2022-10-28 12:10:54 +0200

    Plot all setpoints and validation data

--- PlotsForModeling/plot validation data.py
+++ PlotsForModeling/plot validation data.py
@@ -13,44 +13,32 @@ fault_description = ['Normal operation','Tsuc positive offset','Tsup positive of
 
 #starting dataset
 n = 17
-offset = 0
 
 def filepath(n):#get the filepaths from numbers
     if n == 0:
-        return "TrainingData/Ntrain1.csv"
+        return "ValidationData/NewValid_n1.csv"
     else:
-        return f"TrainingData/Ftrain{n}.csv"
+        return f"ValidationData/NewValid_f{n}.csv"
 
 def on_press(event):
     global n #cuz it only works like this
-    global offset
 
     sys.stdout.flush()
     if event.key == 'left':
         n-=1
-        redraw_Figure(n,offset)
+        redraw_Figure(n)
         
     if event.key == 'right':
         n=n+1
-        redraw_Figure(n,offset)
-
-    if event.key == 'up':
-        offset+=1
-        redraw_Figure(n,offset)
-
-    if event.key == 'down':
-        offset-=1
-        redraw_Figure(n,offset)
+        redraw_Figure(n)
         
-def redraw_Figure(n,offset):
-    start = 0 if offset==0 else offset*6000+offset
-    end = 6000 if offset==0 else (offset+1)*6000+offset
-    data = pd.DataFrame(pd.read_csv(filepath(n))[start:end],dtype=float)
+def redraw_Figure(n):
+    data = pd.DataFrame(pd.read_csv(filepath(n))[0:10000],dtype=float)
     data['CprPower'] = data['CprPower']/1000
     axs.clear()
     axs.plot(data)
     plt.legend(data.columns, bbox_to_anchor=(1, 1))
-    plt.title(f"Fault {n}: {fault_description[n]} \n Case {offset+1}: Tamb={data['Tamb'][start]} Tset={data['Tset'][start]}")
+    plt.title(f'Fault {n}: {fault_description[n]}')
     plt.xlabel('Press left and right arrows to change datasets')
     fig.canvas.draw()
 
@@ -58,10 +46,7 @@ def redraw_Figure(n,offset):
 
 file_name = filepath(n)
 
-start = 0 if offset==0 else offset*6000+offset
-end = 6000 if offset==0 else (offset+1)*6000+offset
-
-data = pd.DataFrame(pd.read_csv(file_name)[start:end],dtype=float)
+data = pd.DataFrame(pd.read_csv(file_name)[0:10000],dtype=float)
 data['CprPower'] = data['CprPower']/1000
 
 fig, axs = plt.subplots(figsize=(10,10))
@@ -69,8 +54,7 @@ fig.canvas.mpl_connect('key_press_event', on_press)
 
 axs.plot(data)
 plt.legend(data.columns, bbox_to_anchor=(1, 1))#, loc="center left")
-plt.title(f"Fault {n}: {fault_description[n]} \n Case {offset+1}: Tamb={data['Tamb'][start]} Tset={data['Tset'][start]}")
+plt.title(f'Fault {n}: {fault_description[n]}')
 plt.xlabel('Press left and right arrows to change datasets')
-
 
 plt.show()
