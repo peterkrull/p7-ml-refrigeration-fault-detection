62bd880ed957ccc895b747ac6b40bfc5ac4007bd
pstefa8 <114411514+pstefa8@users.noreply.github.com>
2022-11-09 16:29:14 +0100

    Update lda.py

--- Plots/LDA_Plots/lda.py
+++ Plots/LDA_Plots/lda.py
@@ -6,7 +6,7 @@ import numpy as np
 class LDA_reducer:
 
     # Initialize the LDA by using a dataset
-    def __init__(self, data : pd.DataFrame, dims : int, target_id : str, frac : int = 1) -> None:
+    def __init__(self, data : pd.DataFrame, dims : int, target_id : str, frac : int = 1, scree_plot : bool = True) -> None:
 
         # Save target id of data
         self.target_id = target_id
@@ -38,41 +38,41 @@ class LDA_reducer:
 
         # Get eigen vectors
         self.eig_val , self.eig_vec = np.linalg.eig(self.omega)
-
         
-    #     # Ensure eigen values are sorted
-    #     idx = np.flip(np.argsort(np.abs(self.eig_val)))
-    #     self.eig_val = self.eig_val[idx]
-    #     self.eig_vec = self.eig_vec[:,idx]
+        # Ensure eigen values are sorted
+        idx = np.flip(np.argsort(np.abs(self.eig_val)))
+        self.eig_val = self.eig_val[idx]
+        self.eig_vec = self.eig_vec[:,idx]
 
-    #     # Save transformation matrix
-    #     self.W = np.real(self.eig_vec[:,0:dims])
+        # Save transformation matrix
+        self.W = np.real(self.eig_vec[:,0:dims])
 
-    #     # Plot largest eigen values
-    #     plt.figure(figsize=(5,5))
-    #     if len(self.eig_vec) > 20:
-    #         plt.bar([x for x in range(1,20+1)],np.real(self.eig_val[0:20]))
-    #     else:
-    #         plt.bar([x for x in range(1,len(self.eig_vec)+1)],np.real(self.eig_val[0:len(self.eig_vec)]))
+        # Plot largest eigen values
+        if scree_plot:
+            plt.figure(figsize=(5,5))
+            if len(self.eig_vec) > 20:
+                plt.bar([x for x in range(1,20+1)],np.real(self.eig_val[0:20]))
+            else:
+                plt.bar([x for x in range(1,len(self.eig_vec)+1)],np.real(self.eig_val[0:len(self.eig_vec)]))
 
-    #     s = sum(np.real(self.eig_val))
-    #     print(f"Preserving {round(sum(np.real(self.eig_val[0:dims]))/s*100,2)}% of variance",)
+        s = sum(np.real(self.eig_val))
+        print(f"Preserving {round(sum(np.real(self.eig_val[0:dims]))/s*100,2)}% of variance",)
     
-    # # Redefine the number of dimensions
-    # def set_dims(self,dims:int):
-    #     self.W = self.eig_vec[:,0:dims].astype('float')
+    # Redefine the number of dimensions
+    def set_dims(self,dims:int):
+        self.W = self.eig_vec[:,0:dims].astype('float')
        
-    # # Transform a data set using the projection matrix
-    # def transform(self, data : pd.DataFrame, target_id : str = None):
+    # Transform a data set using the projection matrix
+    def transform(self, data : pd.DataFrame, target_id : str = None):
 
-    #     # Determine appropriate label definition
-    #     if self.target_id in data: target_id = self.target_id
+        # Determine appropriate label definition
+        if self.target_id in data: target_id = self.target_id
         
-    #     # Do transformation
-    #     if target_id in data:
-    #         Z = pd.DataFrame(data.drop(target_id,axis=1).astype('float').dot(self.W)).assign(target=data[target_id])
-    #     else:
-    #         Z = pd.DataFrame(data.astype('float').dot(self.W))
+        # Do transformation
+        if target_id in data:
+            Z = pd.DataFrame(data.drop(target_id,axis=1).astype('float').dot(self.W)).assign(target=data[target_id])
+        else:
+            Z = pd.DataFrame(data.astype('float').dot(self.W))
         
-    #     # Return data
-    #     return Z.astype('float')
+        # Return data
+        return Z.astype('float')
