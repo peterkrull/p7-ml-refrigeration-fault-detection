9245453c540a619f0dba638c6e48fb68cc49b2f4
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-11-10 14:25:19 +0100

    Update svm_with_gs.py

--- Python/svm_with_gs.py
+++ Python/svm_with_gs.py
@@ -17,8 +17,8 @@ plt.rcParams.update({
 })
 
 # Load training data
-train_data = pd.read_csv("../TrainingData/neodata/fault_all_noise_67.csv")
-test_data = pd.read_csv("../ValidationData/neodata/fault_all_noise_67.csv")
+train_data = pd.read_csv(sys.path[0] + "/../TrainingData/neodata/fault_all_noise_67.csv")
+test_data = pd.read_csv(sys.path[0] + "/../ValidationData/neodata/fault_all_noise_67.csv")
 
 #Normalization / conditioning
 standardizer = sd.standardization(train_data,'target')
@@ -37,6 +37,10 @@ labels_tst = test_data['target']
 parameters = {'kernel':['linear', 'rbf'], 'decision_function_shape':['ovo', 'ovr'], 'C' : [10**x for x in range(-1,6)], 'gamma': [10**x for x in range(-3, 3)]}
 print(parameters)
 svc = svm.SVC()
-clf = GridSearchCV(svc, parameters, verbose = 2)
+clf = GridSearchCV(svc, parameters, verbose = 2, n_jobs=3)
 clf.fit(trn.drop('target', axis = 1).to_numpy(), trn['target'].to_numpy())
 print(clf.best_estimator_)
+
+f = open("svm_grid_search.txt", 'w')
+f.write(str(clf.best_estimator_))
+f.close()
