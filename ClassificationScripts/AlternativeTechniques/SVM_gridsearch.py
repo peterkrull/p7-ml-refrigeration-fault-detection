b5dc7bbc43516d05011525839ae0f42773791ff3
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-11-24 14:32:12 +0100

    Update SVM_gridsearch.py

--- ClassificationScripts/AlternativeTechniques/SVM_gridsearch.py
+++ ClassificationScripts/AlternativeTechniques/SVM_gridsearch.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.svm as svm
-from sklearn.model_selection import GridSearchCV, PredefinedSplit
+from sklearn.model_selection import GridSearchCV, PredefinedSplit,GroupKFold
 import sys
 sys.path.append(sys.path[0] + "/../../Python")
 import confusion_matrix2 as confusion_matrix
 import standardization 
 from datetime import datetime
+from joblib import dump, load
+
 
 def false_positives(true_target : pd.DataFrame, predicted_target : pd.DataFrame):
     zero_label_index = true_target.loc[true_target['target']==0]
@@ -41,16 +43,18 @@ if __name__ == "__main__":
     val_data = std.transform(val_data)
     test_data = std.transform(test_data)
 
-    val_data = val_data
-    train_data1 = train_data1
+    val_data = val_data.iloc[::10,:]
+    train_data1 = train_data1.iloc[::10,:]
 
     val_fold = [-1 for _ in range(len(train_data1)) ] + [0 for _ in range(len(val_data))]    
 
     optimize_data = pd.concat([train_data1, val_data])
 
     svc = svm.SVC()
-    C_params = [10**x for x in np.linspace(1,5, 101)]
-    gamma_params = [10**x for x in np.linspace(-4,0, 101)]
+    #C_params = [10**x for x in np.linspace(0,5, 121)]
+    #gamma_params = [10**x for x in np.linspace(-4,-1, 81)]
+    C_params = [10**x for x in np.linspace(0,5, 3)]
+    gamma_params = [10**x for x in np.linspace(-4,-1, 3)]
     score = make_scorer(gridsearch_scoring, greater_is_better= True)
     ps = PredefinedSplit(val_fold)
     clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3, scoring = score, cv = ps)
@@ -66,10 +70,41 @@ if __name__ == "__main__":
     f = open(sys.path[0] + "/optimum_Svm/svm_grid_search.txt", 'w')
     f.write(str(datetime.now()) + "\n\n")
     f.write(str(clf.cv_results_))
-    f.write(str(clf.best_estimator_))
+    f.write("\n\n" + str(clf.best_estimator_))
     f.close()
 
     cv_log = pd.DataFrame.from_dict(clf.cv_results_)
     f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log.json", 'w')
     f.write(cv_log.to_json())
     f.close()
+    dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest.joblib')
+
+
+    ##KFOLD-CROSSVALIDATION##
+
+    svc = svm.SVC()
+    C_params = [10**x for x in np.linspace(0,5, 121)]
+    gamma_params = [10**x for x in np.linspace(-4,-1, 81)]
+    score = make_scorer(gridsearch_scoring, greater_is_better= True)
+    ps = PredefinedSplit(val_fold)
+    clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3, scoring = score, cv =GroupKFold(n_splits=5))
+    clf.fit(optimize_data.drop('target', axis = 1).to_numpy(), optimize_data['target'].to_numpy())
+
+    tst_pred = clf.predict(test_data.drop('target', axis = 1).to_numpy())
+
+    confusion_matrix.confusion_matrix(test_data['target'],tst_pred, save_fig_name = sys.path[0] + "/optimum_Svm/optimum_svm_tst_kfold.pdf")
+
+    print("Best estimator = " + str(clf.best_estimator_))
+
+    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_kfold.txt", 'w')
+    f.write(str(datetime.now()) + "\n\n")
+    f.write(str(clf.cv_results_))
+    f.write("\n\n" + str(clf.best_estimator_))
+    f.close()
+
+    cv_log = pd.DataFrame.from_dict(clf.cv_results_)
+    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log_kfold.json", 'w')
+    f.write(cv_log.to_json())
+    f.close()
+
+    dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest_kfold.joblib')
