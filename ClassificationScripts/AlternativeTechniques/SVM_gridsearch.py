6398eb4ce9fd0597cc195dcabfdbebceeb8bea3a
soerenLang <58945910+soerenLang@users.noreply.github.com>
2022-11-29 09:42:57 +0100

    Update SVM_gridsearch.py

--- ClassificationScripts/AlternativeTechniques/SVM_gridsearch.py
+++ ClassificationScripts/AlternativeTechniques/SVM_gridsearch.py
@@ -50,34 +50,34 @@ if __name__ == "__main__":
 
     optimize_data = pd.concat([train_data1, val_data])
 
-    svc = svm.SVC()
-    C_params = [10**x for x in np.linspace(0,5, 121)]
-    gamma_params = [10**x for x in np.linspace(-4,-1, 81)]
-    #C_params = [10**x for x in np.linspace(0,5, 3)]
-    #gamma_params = [10**x for x in np.linspace(-4,-1, 3)]
-    score = make_scorer(gridsearch_scoring, greater_is_better= True)
-    ps = PredefinedSplit(val_fold)
-    clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3, scoring = score, cv = ps)
-    #clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3)
-    clf.fit(optimize_data.drop('target', axis = 1).to_numpy(), optimize_data['target'].to_numpy())
+    # svc = svm.SVC()
+    # C_params = [10**x for x in np.linspace(0,5, 121)]
+    # gamma_params = [10**x for x in np.linspace(-4,-1, 81)]
+    # #C_params = [10**x for x in np.linspace(0,5, 3)]
+    # #gamma_params = [10**x for x in np.linspace(-4,-1, 3)]
+    # score = make_scorer(gridsearch_scoring, greater_is_better= True)
+    # ps = PredefinedSplit(val_fold)
+    # clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3, scoring = score, cv = ps)
+    # #clf = GridSearchCV(svc, {'kernel':['rbf'], 'decision_function_shape':['ovo'], 'C' : C_params, 'gamma' : gamma_params}, n_jobs = -1, verbose = 3)
+    # clf.fit(optimize_data.drop('target', axis = 1).to_numpy(), optimize_data['target'].to_numpy())
 
-    tst_pred = clf.predict(test_data.drop('target', axis = 1).to_numpy())
+    # tst_pred = clf.predict(test_data.drop('target', axis = 1).to_numpy())
 
-    confusion_matrix.confusion_matrix(test_data['target'],tst_pred, save_fig_name = sys.path[0] + "/optimum_Svm/optimum_svm_tst.pdf")
+    # confusion_matrix.confusion_matrix(test_data['target'],tst_pred, save_fig_name = sys.path[0] + "/optimum_Svm/optimum_svm_tst.pdf")
 
-    print("Best estimator = " + str(clf.best_estimator_))
+    # print("Best estimator = " + str(clf.best_estimator_))
 
-    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search.txt", 'w')
-    f.write(str(datetime.now()) + "\n\n")
-    f.write(str(clf.cv_results_))
-    f.write("\n\n" + str(clf.best_estimator_))
-    f.close()
+    # f = open(sys.path[0] + "/optimum_Svm/svm_grid_search.txt", 'w')
+    # f.write(str(datetime.now()) + "\n\n")
+    # f.write(str(clf.cv_results_))
+    # f.write("\n\n" + str(clf.best_estimator_))
+    # f.close()
 
-    cv_log = pd.DataFrame.from_dict(clf.cv_results_)
-    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log.json", 'w')
-    f.write(cv_log.to_json())
-    f.close()
-    dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest.joblib')
+    # cv_log = pd.DataFrame.from_dict(clf.cv_results_)
+    # f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log.json", 'w')
+    # f.write(cv_log.to_json())
+    # f.close()
+    # dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest2.joblib')
 
 
     ##KFOLD-CROSSVALIDATION##
@@ -93,9 +93,9 @@ if __name__ == "__main__":
     train_data2 = std.transform(train_data2)
     test_data = std.transform(test_data)
 
-
+    
     svc = svm.SVC()
-    C_params = [10**x for x in np.linspace(0,5, 121)]
+    C_params = [10**x for x in np.linspace(4,7, 31)]
     gamma_params = [10**x for x in np.linspace(-4,-1, 81)]
     #C_params = [10**x for x in np.linspace(0,5, 3)]
     #gamma_params = [10**x for x in np.linspace(-4,-1, 3)]
@@ -106,19 +106,19 @@ if __name__ == "__main__":
 
     tst_pred = clf.predict(test_data.drop('target', axis = 1).to_numpy())
 
-    confusion_matrix.confusion_matrix(test_data['target'],tst_pred, save_fig_name = sys.path[0] + "/optimum_Svm/optimum_svm_tst_kfold.pdf")
+    confusion_matrix.confusion_matrix(test_data['target'],tst_pred, save_fig_name = sys.path[0] + "/optimum_Svm/optimum_svm_tst_kfold2.pdf")
 
     print("Best estimator = " + str(clf.best_estimator_))
 
-    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_kfold.txt", 'w')
+    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_kfold2.txt", 'w')
     f.write(str(datetime.now()) + "\n\n")
     f.write(str(clf.cv_results_))
     f.write("\n\n" + str(clf.best_estimator_))
     f.close()
 
     cv_log = pd.DataFrame.from_dict(clf.cv_results_)
-    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log_kfold.json", 'w')
+    f = open(sys.path[0] + "/optimum_Svm/svm_grid_search_log_kfold2.json", 'w')
     f.write(cv_log.to_json())
     f.close()
 
-    dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest_kfold.joblib')
+    dump(clf,sys.path[0] + '/optimum_Svm/GridSearchBest_kfold2.joblib')
